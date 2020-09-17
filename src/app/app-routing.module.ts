e2fed01c5598a6f06389f4aed4a956cf0a22a274
ebrahimahmed97090 <ebrahimahmed97090@gmail.com>
2020-09-18 01:24:18 +0200

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,57 @@
+import { TranslationsComponent } from './container/translations/translations.component';
+import { ManageAccessComponent } from './container/manage-access/manage-access.component';
+import { TagsComponent } from './container/tags/tags.component';
+import { BulkActionsComponent } from './container/bulk-actions/bulk-actions.component';
+import { WithRelationshipComponent } from './container/with-relationship/with-relationship.component';
+import { WithExportComponent } from './container/with-export/with-export.component';
+import { WithMediaComponent } from './container/with-media/with-media.component';
+import { StandardComponent } from './container/standard/standard.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { TranslatableComponent } from './container/translatable/translatable.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'standard', pathMatch: 'full' },
+  {
+    path: 'standard',
+    component: StandardComponent,
+  },
+  {
+    path: 'media',
+    component: WithMediaComponent,
+  },
+  {
+    path: 'export',
+    component: WithExportComponent,
+  },
+  {
+    path: 'relation',
+    component: WithRelationshipComponent,
+  },
+  {
+    path: 'bulk',
+    component: BulkActionsComponent,
+  },
+  {
+    path: 'tags',
+    component: TagsComponent,
+  },
+  {
+    path: 'accessman',
+    component: ManageAccessComponent,
+  },
+  {
+    path: 'transaltions',
+    component: TranslationsComponent,
+  },
+  {
+    path: 'translatable',
+    component: TranslatableComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
